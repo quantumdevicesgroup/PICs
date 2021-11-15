51e74c0b62f7b1364d023ca068b80d8c49e7e877
Lukasz Komza <lukasz.komza@gmail.com>
2021-11-15 10:37:52 -0800

    more realistic test structures for HSiQ

--- HSiQ_test_11-12-2021/HSiQ dose tests 11-12-2021.py
+++ HSiQ_test_11-12-2021/HSiQ dose tests 11-12-2021.py
@@ -1,6 +1,10 @@
 import sys
-sys.path.append("C:\\Users\\lukas\\Desktop\\QDG_PICs")
-from chip_class import *
+try:
+	sys.path.append("C:\\Users\\lukas\\Desktop\\QDG_PICs")
+	from chip_class import *
+except:
+	sys.path.append("C:\\Users\\Lukasz\\Desktop\\QDG_PICs")
+	from chip_class import *
 import numpy as np
 cm,mm,um,nm = 10**4,10**3,1,10**-3
 
@@ -8,24 +12,20 @@ chip = Chip(1*cm, 1*cm, 600*um)
 chip.create_wgt(300*nm,0*um,20*um)
 #chip.deep_etch(1*mm)
 
+chip.etch_markers([(5*mm-25*um,5*mm+25*um)],50*um, 3*um)
+chip.etch_markers([(5*mm-175*um,5*mm+175*um)],250*um, 10*um)
 
-chip.etch_markers([(6*mm,6*mm)],200*um, 10*um)
+chip.etch_markers([(6*mm-240*um,5*mm-35*um)],50*um, 3*um)
+chip.etch_markers([(6*mm-90*um,5*mm-175*um)],250*um, 10*um)
 
 write_layer = 30
-for i in range(20):
+for i in range(30):
 	chip.change_write_layer(write_layer)
 	x_s, y_s = 5*mm,5*mm
-	x,y=chip.etch_wg([(x_s+i*10*um,y_s),(x_s+i*10*um,y_s-10*um)])
-	#x,y=chip.etch_wg((x_s+i*10*um,y_s),(310*nm*15),0,310*nm,0.5,220*nm)
+	chip.etch_wg([(x_s+i*25*um,y_s),(x_s+i*25*um+10*um,y_s)])
+	chip.etch_wg([(x_s+i*25*um,y_s-5*um),(x_s+i*25*um+10*um,y_s-5*um)])
+	chip.etch_saw_bragg_ref((x_s+i*25*um,y_s-10*um),(480*nm*10),480*nm*5,480*nm,0.5,100*nm,w=300*nm)
+	#chip.etch_label((x_s+i*25*um,y_s-20*um),5*um,str(50+i*50))
 	write_layer += 1
-
-# write_layer = 30
-# for i in range(19):
-# 	x_i = x_s+(i-9.25)*30*um
-# 	for j in range(5):
-# 		chip.change_write_layer(write_layer)
-# 		x,y=chip.etch_saw_bragg_ref((x_i,y_s+(j-2)*2*um),(310*nm*50),0,310*nm,0.5,220*nm)
-# 	chip.etch_label((x+1*um,y-0.5*um),1*um,str(500+i*250))
-# 	write_layer += 1
 
 chip.build('HSiQ_dose_calib')
